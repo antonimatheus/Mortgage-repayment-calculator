8f7016eb125c73492629c9942d11f81ef02729b7
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-08-06 17:18:44 -0400

    Created Main file (Form/Results)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
+import Results from './Main/Results'
+import Form from './Main/Form'
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Form />
+      <Results />
     </>
   )
 }
